3ae93c5baaaec5ceac4bdc156ec47f6518b55308
ibozorus <korkmazie@gmail.com>
2023-11-19 00:06:13 +0100

    - zorotrozr

--- popup.js
+++ popup.js
@@ -1,21 +1,3 @@
-// // Assume you have a simple database or static data
-// const database = [
-//     { id: 1, name: 'Item 1' },
-//     { id: 2, name: 'Item 2' },
-//     // Add more data as needed
-// ];
-//
-// // Function to render database items
-// function renderDatabase() {
-//     const container = document.querySelector('.container');
-//     container.innerHTML = '<h1>DB Navigator</h1>';
-//
-//     database.forEach(item => {
-//         const listItem = document.createElement('div');
-//         listItem.textContent = `${item.id}: ${item.name}`;
-//         container.appendChild(listItem);
-//     });
-// }
-//
-// // Call the render function when the popup is loaded
-// document.addEventListener('DOMContentLoaded', renderDatabase);
+$(function() {
+    $("#datepicker").datepicker();
+});
