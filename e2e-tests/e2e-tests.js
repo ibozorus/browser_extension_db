947f88e6e8dc5c549473a88c4258bbc513088455
ibozorus <korkmazie@gmail.com>
2024-01-16 09:09:59 +0100

    Tests angepasst

--- e2e-tests/e2e-tests.js
+++ e2e-tests/e2e-tests.js
@@ -7,13 +7,13 @@ const {Builder, By, until} = require("selenium-webdriver");
 describe('First script', function () {
     let driver;
     this.timeout(50000)
-
+    const path = "file:///C:/Users/iekorkmaz/WebstormProjects/browser_extension_db/popup.html"
     before(async function () {
         driver = await new Builder().forBrowser('chrome').build();
     });
 
     it('Verbindunssuche Elemente Displayed Test', async function () {
-        await driver.get('file:///C:/Users/iekorkmaz/WebstormProjects/browser_extension_db/popup.html');
+        await driver.get(path);
         let homePage = await driver.findElement(By.css('#homepage'));
         let dropDownMenu = await driver.findElement(By.css('#dropdownMenuButton1'));
         let vonInput = await driver.findElement(By.css('#von-input'));
@@ -28,7 +28,7 @@ describe('First script', function () {
         assert.equal(true, await suchenButton.isDisplayed());
     });
     it('Verbindunssuche Interaktion Test', async function () {
-        await driver.get('file:///C:/Users/iekorkmaz/WebstormProjects/browser_extension_db/popup.html');
+        await driver.get(path);
         let vonInput = await driver.findElement(By.css('#von-input'));
         let vonInputModal = await driver.findElement(By.css('#von-input-modal'));
         let stationTauschen = await driver.findElement(By.css('#station-tauschen'));
@@ -50,7 +50,7 @@ describe('First script', function () {
         vonInputModal.sendKeys("a");
         vonInputModal.sendKeys("r");
         await driver.sleep(5000)
-        await driver.findElement(await By.css("#von-liste > li:first-child")).click();
+        await driver.findElement(await By.css("#von-liste > div:first-child")).click();
         nachInput.click();
         nachInputModal.click();
         nachInputModal.sendKeys("E");
@@ -60,7 +60,7 @@ describe('First script', function () {
         nachInputModal.sendKeys("r");
         nachInputModal.sendKeys("t");
         await driver.sleep(5000);
-        await driver.findElement(await By.css("#nach-liste > li:first-child")).click();
+        await driver.findElement(await By.css("#nach-liste > div:first-child")).click();
         await driver.findElement(await By.css("#suchen-button")).click();
         await driver.sleep(5000);
         assert.equal(true, await driver.findElement(By.css("#result-items")).isDisplayed());
@@ -83,6 +83,27 @@ describe('First script', function () {
         maxTransfers.click();
         maxTransfers.sendKeys(1);
         await driver.findElement(By.css("#apply-options")).click();
+    });
+    it('Fahrplan Test', async function () {
+        await driver.get(path);
+        let dropDownMenu = await driver.findElement(By.css('#dropdownMenuButton1'));
+        let wechselFahrplan = await driver.findElement(By.css('#wechsel-fahrplan'));
+        let haltestelleInput = await driver.findElement(By.css("#haltestelle-input"));
+        let vonInput = await driver.findElement(By.css("#von-input"));
+        dropDownMenu.click();
+        wechselFahrplan.click();
+        await driver.sleep(1000)
+        haltestelleInput.click();
+        await driver.sleep(1000)
+        vonInput.click();
+        vonInput.sendKeys("W");
+        vonInput.sendKeys("e");
+        vonInput.sendKeys("i");
+        vonInput.sendKeys("m");
+        vonInput.sendKeys("a");
+        vonInput.sendKeys("r");
+        await driver.sleep(1000);
+        //await driver.findElement(await By.css("#von-liste > li:first-child")).click();
     });
 
     after(async () => await driver.quit());
